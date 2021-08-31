865fa278356c719f554f915ca60dc1804950f5f9
Oleg Valter <o.a.philippov@gmail.com>
2021-08-31 18:39:46 +0300

    fixed declaration import

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,7 @@
 // Minimum TypeScript Version: 4.1
-
-import "@stackoverflow/stacks/dist/js/stacks.js";
 import "jquery";
-import "./lib/chat.d.ts";
-import "./lib/stackexchange.d.ts";
-import "./lib/stacks.d.ts";
-
-declare global {
-    Stacks;
-    StackExchange;
-    CHAT;
-}
+import "./lib/chat";
+import "./lib/stackexchange";
+import "./lib/stacks";
 
 export {};
