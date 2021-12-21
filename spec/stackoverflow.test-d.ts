2a91fc68b3c13c48c88290f0e45e9e2dc7fcb931
Oleg Valter <o.a.philippov@gmail.com>
2021-12-22 02:38:29 +0300

    added more tests for the StackOverflow global object

--- spec/stackoverflow.test-d.ts
+++ spec/stackoverflow.test-d.ts
@@ -6,3 +6,15 @@ Object.values(StackOverflow.Models.UserFlags).forEach((val) => {
         expectAssignable<StackOverflow.Models.UserFlags>(val);
     }
 });
+
+expectAssignable<StackOverflow.Models.NotificationFrequencyTypeId>(
+    StackOverflow.Models.NotificationFrequencyTypeId.Daily
+);
+
+expectAssignable<StackOverflow.Models.AccountPreferenceFlags>(
+    StackOverflow.Models.AccountPreferenceFlags.HideLeftNavigation
+);
+
+expectAssignable<StackOverflow.Models.PostTypeId>(
+    StackOverflow.Models.PostTypeId.Answer
+);
