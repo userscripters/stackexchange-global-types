0e01ef8410c4ac04f25540162e570acb66b5e8b2
Oleg Valter <o.a.philippov@gmail.com>
2023-02-27 22:16:59 +0300

    added tests for Stacks controller classes

--- spec/stacks.test-d.ts
+++ spec/stacks.test-d.ts
@@ -4,6 +4,23 @@ import "../lib/stacks";
 
 // We don't need to test Stacks itself, just that some of the expected
 // objects and types exist, as a global namespace.
+
 expectType<Stacks.StacksApplication>(Stacks.application);
 
-export {}
+expectType<typeof Stacks.BannerController>(Stacks.BannerController);
+
+expectType<typeof Stacks.ExpandableController>(Stacks.ExpandableController);
+
+expectType<typeof Stacks.ModalController>(Stacks.ModalController);
+
+expectType<typeof Stacks.PopoverController>(Stacks.PopoverController);
+
+expectType<typeof Stacks.TabListController>(Stacks.TabListController);
+
+expectType<typeof Stacks.TooltipController>(Stacks.TooltipController);
+
+expectType<typeof Stacks.ToastController>(Stacks.ToastController);
+
+expectType<typeof Stacks.StacksController>(Stacks.StacksController);
+
+expectType<typeof Stacks.UploaderController>(Stacks.UploaderController);
